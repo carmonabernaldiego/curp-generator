61ff7a33029d8e31d1103f8d68de399b8c967411
Diego Bernal <diego.carmona@xpertme.com>
2024-04-08 11:05:16 -0600

    update

--- resources/views/livewire/curp.blade.php
+++ resources/views/livewire/curp.blade.php
@@ -39,7 +39,7 @@
         <div class="form-group">
             <label for="primer_apellido" class="mt-3">Primer apellido:</label>
             <input type="text" class="form-control" wire:model="primer_apellido" id="primer_apellido"
-                   name="primer_apellido">
+                name="primer_apellido">
             <div class="invalid-feedback" id="primerApellidoError" style="display: none;">Por favor, ingrese el
                 primer apellido.
             </div>
@@ -47,7 +47,7 @@
         <div class="form-group">
             <label for="segundo_apellido" class="mt-3">Segundo apellido:</label>
             <input type="text" class="form-control" wire:model="segundo_apellido" id="segundo_apellido"
-                   name="segundo_apellido">
+                name="segundo_apellido">
             <div class="invalid-feedback" id="segundoApellidoError" style="display: none;">Por favor, ingrese el
                 segundo apellido.
             </div>
@@ -55,7 +55,7 @@
         <div class="form-group">
             <label for="fecha_nacimiento" class="mt-3">Fecha de nacimiento: día, mes, año.</label>
             <input type="date" class="form-control" wire:model="fecha_nacimiento" id="fecha_nacimiento"
-                   name="fecha_nacimiento">
+                name="fecha_nacimiento">
             <div class="invalid-feedback" id="fechaNacimientoError" style="display: none;">Por favor, ingrese la fecha
                 de nacimiento.
             </div>
@@ -72,9 +72,9 @@
         <div class="form-group">
             <label for="entidad_nacimiento" class="mt-3">Entidad de Nacimiento:</label>
             <select class="form-control" wire:model="entidad_nacimiento" id="entidad_nacimiento"
-                    name="entidad_nacimiento">
+                name="entidad_nacimiento">
                 <option value="" selected>Seleccione</option>
-                @foreach($entidades as $entidad)
+                @foreach ($entidades as $entidad)
                     <option value="{{ $entidad->codigo }}">{{ $entidad->nombre }}</option>
                 @endforeach
             </select>
@@ -86,7 +86,7 @@
             <label for="codigo_verificacion" class="mt-3">Código de
                 Verificación:</label>
             <input type="text" class="form-control" id="codigo_verificacion" name="codigo_verificacion"
-                   autocomplete="off">
+                autocomplete="off">
             <div class="invalid-feedback" id="verificacionError" style="display: none;">Por favor, ingrese el código de
                 verificación correcto.
             </div>
@@ -103,6 +103,15 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="{{ asset('js/curp.js') }}"></script>
 <script>
+    function validarTexto(texto) {
+        return /^[A-Za-záéíóúÁÉÍÓÚ\s]+$/.test(texto);
+    }
+
+    // Función para validar una fecha en el formato YYYY-MM-DD
+    function validarFecha(fecha) {
+        return !isNaN(Date.parse(fecha));
+    }
+
     // Función para generar un código de verificación aleatorio
     function generarCodigoVerificacion() {
         var longitud = 6;
@@ -146,7 +155,7 @@
         var entidadNacimiento = document.getElementById('entidad_nacimiento').value.trim();
         var formularioValido = true; // Variable para verificar si el formulario es válido
 
-        if (nombre === '') {
+        if (!validarTexto(nombre)) {
             document.getElementById('nombre').classList.add('is-invalid');
             document.getElementById('nombreError').style.display = 'block';
             formularioValido = false;
@@ -155,7 +164,7 @@
             document.getElementById('nombreError').style.display = 'none';
         }
 
-        if (apellidoPaterno === '') {
+        if (!validarTexto(apellidoPaterno)) {
             document.getElementById('primer_apellido').classList.add('is-invalid');
             document.getElementById('primerApellidoError').style.display = 'block';
             formularioValido = false;
@@ -164,7 +173,7 @@
             document.getElementById('primerApellidoError').style.display = 'none';
         }
 
-        if (apellidoMaterno === '') {
+        if (!validarTexto(apellidoMaterno)) {
             document.getElementById('segundo_apellido').classList.add('is-invalid');
             document.getElementById('segundoApellidoError').style.display = 'block';
             formularioValido = false;
@@ -173,37 +182,19 @@
             document.getElementById('segundoApellidoError').style.display = 'none';
         }
 
-        if (fechaNacimiento === '') {
+        if (!fechaNacimiento || fechaNacimiento === "0000-00-00") {
             document.getElementById('fecha_nacimiento').classList.add('is-invalid');
             document.getElementById('fechaNacimientoError').style.display = 'block';
             formularioValido = false;
         } else {
-            document.getElementById('fecha_nacimiento').classList.remove('is-invalid');
-            document.getElementById('fechaNacimientoError').style.display = 'none';
-        }
-
-        if (sexo === '') {
-            document.getElementById('sexo').classList.add('is-invalid');
-            document.getElementById('sexoError').style.display = 'block';
-            formularioValido = false;
-        } else {
-            document.getElementById('sexo').classList.remove('is-invalid');
-            document.getElementById('sexoError').style.display = 'none';
-        }
-
-        if (entidadNacimiento === '') {
-            document.getElementById('entidad_nacimiento').classList.add('is-invalid');
-            document.getElementById('entidadError').style.display = 'block';
-            formularioValido = false;
-        } else {
-            document.getElementById('entidad_nacimiento').classList.remove('is-invalid');
-            document.getElementById('entidadError').style.display = 'none';
-        }
-
-        // Si el formulario no es válido, no continuar con la generación de la CURP
-        if (!formularioValido) {
-            mostrarCodigoVerificacion();
-            return;
+            if (!validarFecha(fechaNacimiento)) {
+                document.getElementById('fecha_nacimiento').classList.add('is-invalid');
+                document.getElementById('fechaNacimientoError').style.display = 'block';
+                formularioValido = false;
+            } else {
+                document.getElementById('fecha_nacimiento').classList.remove('is-invalid');
+                document.getElementById('fechaNacimientoError').style.display = 'none';
+            }
         }
 
         if (verificarCodigo()) {
@@ -214,7 +205,8 @@
                 apellido_materno: apellidoMaterno,
                 sexo: sexo,
                 estado: entidadNacimiento,
-                fecha_nacimiento: fechaNacimiento.split('-').reverse().map(Number) // Convierte la fecha en un array [día, mes, año]
+                fecha_nacimiento: fechaNacimiento.split('-').reverse().map(
+                    Number) // Convierte la fecha en un array [día, mes, año]
             });
 
             // Mostrar la CURP en un Sweet Alert
@@ -246,4 +238,3 @@
     // Llamar a la función para mostrar el código de verificación al cargar la página
     mostrarCodigoVerificacion();
 </script>
-
